5abbe83a828b125917b5105d09c09b6fe130540a
Leo Zeitler <leozeitler95@gmail.com>
2021-03-24 19:01:12 +0100

    Update: Slamdunk entry point to slamdunk_main.py
    Changed SlAMdunk entry point to slamdunk_main as this makes it
    easier to develop in the test environment. This can be changed
    back later. However, it can create problems when the module
    script has the same name as the package.

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
     'console_scripts': [
-        'slamdunk=slamdunk.slamdunk:run',
+        'slamdunk=slamdunk.slamdunk_main:run',
         'alleyoop=slamdunk.alleyoop:run',
         'splash=slamdunk.splash:run',
     ],
